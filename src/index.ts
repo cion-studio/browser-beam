c0ecd59160b80151828b1095d52c6c46d1a55c24
youssof707 <youssoffahmy@gmail.com>
2023-05-28 16:15:13 -0400

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import beamCreator from './beamCreator'
 
 //For creating a custom Beam object with tokenBuilder and urlPrefix
 export default beamCreator(fetch)
-export beamCreator
+export {
+	beamCreator
+}
 
 //For just using the request functions directly
 const BeamClass = beamCreator(fetch)
